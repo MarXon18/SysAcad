7b066f7f421bc9540b18ba6b09921409c578f512
Luanna Guajardo <luannag42@gmail.com>
2025-12-02 12:38:46 -0300

    decorador para peticiones JSON

---
+++ app/validators/validators.py
@@ -0,0 +1,14 @@
+from flask import jsonify, request
+from functools import wraps
+from marshmallow import ValidationError
+
+def validate_with(schema):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            try:
+                data = schema().load(request.json)
+            except ValidationError as err:
+                return jsonify(err.messages),400
+            return f(data, *args, **kwargs)
+        return decorator
