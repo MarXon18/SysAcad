240f5fe220c44ac00877b20d68de903b09d30b0c
Luanna Guajardo <luannag42@gmail.com>
2025-12-02 12:41:42 -0300

    mapping universidad

--- app/mapping/universidad_mapping.py
+++ app/mapping/universidad_mapping.py
@@ -1,12 +1,16 @@
 from marshmallow import Schema, fields, post_load, validate
 from app.models import Universidad
+from markupsafe import escape
 
 class UniversidadMapping(Schema):
-    hashid = fields.String(dump_only=True)
+    hashids = fields.String(attribute="hashid",dump_only=True)
     nombre = fields.String(required=True, validate=validate.Length(min=1, max=100))
     sigla = fields.String(required=True, validate=validate.Length(min=1, max=5))
-    facultad_id = fields.Integer(allow_none=True)   
+    #facultad_id = fields.Integer(allow_none=True)   
 
     @post_load
     def nueva_universidad(self, data, **kwargs): 
+        for key in ['nombre', 'sigla']:
+            if key in data:
+                data[key] = escape(data[key])
         return Universidad(**data)
