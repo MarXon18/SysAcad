c6ef5548bfd0f76ba10c1b262749b316672eff58
Code7Zero <96557825+Code7Zero@users.noreply.github.com>
2025-12-02 02:00:15 -0300

    Update materia.py

--- app/models/materia.py
+++ app/models/materia.py
@@ -1,21 +1,3 @@
-'''from dataclasses import dataclass  
-from app.models.nota import Nota
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, ForeignKey
-from app import db
-
-@dataclass(init=False, repr=True, eq=True)
-class Materia(db.Model):
-    __tablename__ = 'materias'
-    
-    id: int = db.Column(db.Integer, primary_key=True)
-    nombre: str = db.Column(db.String(100), nullable= False)
-    codigo: str = db.Column(db.String(100), nullable= False)
-    observacion: str = db.Column(db.String(100), nullable= False)
-    
-    nota_id = db.Column(db.Integer, db.ForeignKey('notas.id'), nullable=True)
-    nota = db.relationship('Nota', backref='materias')
-    '''
 from dataclasses import dataclass  
 from app.models.nota import Nota
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -33,3 +15,4 @@ class Materia(db.Model):
 
     nota_id: Mapped[int] = mapped_column(ForeignKey('notas.id'), nullable=True)
     nota: Mapped["Nota"] = relationship('Nota', backref='materias')
+
