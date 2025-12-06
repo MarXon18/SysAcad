86fe5bd37f1b65a5c8cf330d8c48e5ec8a946e4e
lucasspc144 <164245666+lucasspc144@users.noreply.github.com>
2025-12-06 06:33:38 -0300

    Refactorizado de config
    Removed unused environment variable configurations and added database connection details for production.

--- app/config/config.py
+++ app/config/config.py
@@ -10,11 +10,6 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
-    #AGREGUE
-    HASHIDS_MIN_LENGTH : str | None = os.environ.get('HASHIDS_MIN_LENGTH')
-    HASHIDS_ALPHABET : str | None = os.environ.get('HASHIDS_ALPHABET')
-    HASHIDS_SALT : str | None = os.environ.get('HASHIDS_SALT')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
     
     @staticmethod
     def init_app(app):
@@ -31,12 +26,18 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
+    
+
         
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_RECORD_QUERIES = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
+    user = os.environ.get('USER_DB')
+    password = os.environ.get('PASSWORD_DB')
+    host = os.environ.get('HOST_DB')
+    db_name = os.environ.get('NAME_DB')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{user}:{password}@{host}/{db_name}"
     
     @classmethod
     def init_app(cls, app):
